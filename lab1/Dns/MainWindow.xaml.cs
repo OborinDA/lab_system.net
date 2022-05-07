ba93a4eabe9cb979f89bc00ab627fb57f2ec7607
Даниил Оборин <allinahell@gmail.com>
2022-05-07 21:21:48 +0300

    Use dns class

---
+++ lab1/Dns/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Windows;
+
+namespace Dns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string hostname = "www.google.com", message = "IP адреса для домена " + hostname + "\n";
+            IPHostEntry entry = System.Net.Dns.GetHostEntry(hostname);
+
+            foreach (IPAddress a in entry.AddressList)
+                message += "  --> " + a.ToString() + "\n";
+
+            message += "\nАльтернативное имя домена: ";
+            foreach (string aliasName in entry.Aliases)
+                message += aliasName + "\n";
+
+            message += "\nРеальное название хоста: " + entry.HostName;
+            MessageBox.Show(message);
+        }
+    }
+}
